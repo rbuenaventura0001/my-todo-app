12a5f56989965803a273253b86b347e077785fa5
ralph <rbuenaventura00001@gmail.com>
2024-01-28 15:52:31 -0600

    Initial commit

--- webr.py
+++ webr.py
@@ -4,6 +4,9 @@ import functions
 todos = functions.get_todos()
 
 
+# st.set_page_config(layout="wide")
+# can use this to configure web app
+
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
@@ -12,7 +15,17 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity.")
+st.write("This app is to increase your <b>productivity</b>."
+         " The way to use this app is to enter your task for the day in the input box below and press the enter key."
+         " If you would like to complete the task simply click the checkbox next to the task you want to complete."
+         " This is a group effort so don't complete the task unless it is agreed upon. A chatbox will be added later so"
+         "we can use that to communicate. Thanks for using the app. Let's get going!",
+         unsafe_allow_html=True)
+
+# can use html code to customize web app
+
+st.text_input(label="Enter a todo", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -22,7 +35,6 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="Enter a todo", placeholder="Add new todo...",
-              on_change=add_todo, key='new_todo')
+
 
 # changes to file
